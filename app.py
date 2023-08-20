c29e243080baa52cc9c5e55cb6afbff3699d52c0
Marios <marios_2510@hotmail.com>
2023-08-20 15:03:23 +0300

    Initial commit. Basic app.py and index.html. Create db. Create a model for the todos. Create basic routing.

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+# Sample data structure for todo items
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    task = request.form.get('task')
+    if task:
+        todos.append({"task": task, "completed": False})
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:index>')
+def complete_todo(index):
+    if 0 <= index < len(todos):
+        todos[index]['completed'] = True
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
