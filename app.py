aa6df39e39d255b56848c05619cb0e27e9112ded
Marios <marios_2510@hotmail.com>
2023-08-22 13:47:33 +0300

    Create register.html and login.html. Modify index.html to display register or login. Create boards.html to display boards of the current user. Update base.html to check for authenticated users. Update forms.py to add Registration and Login forms. Modify app.py to add user authentication and also update models and routes.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from forms import TodoForm, BoardForm, TaskForm
 from flask_bootstrap import Bootstrap5
 from flask import jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from wtforms.validators import ValidationError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SECRET_KEY'] = 'Secret_Key'
 Bootstrap5(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 db = SQLAlchemy()
 db.init_app(app)
@@ -16,6 +25,7 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     columns = db.relationship('Column', backref='board', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Column(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,19 +39,91 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean, default=False)
     column_id = db.Column(db.Integer, db.ForeignKey('column.id'), nullable=False)
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    boards = db.relationship('Board', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+from forms import TodoForm, BoardForm, TaskForm, RegistrationForm, LoginForm
+
+def validate_username(form, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user is not None:
+        raise ValidationError('Username already in use.')
+
+def validate_email(form, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user is not None:
+        raise ValidationError('Email already registered.')
+
+RegistrationForm.validate_username = validate_username
+RegistrationForm.validate_email = validate_email
+
+
 
 @app.route('/')
 def index():
-    results = db.session.execute(db.select(Board)).scalars()
-    boards = results.all()
-    return render_template("index.html", all_boards=boards)
+    if current_user.is_authenticated:
+        return redirect(url_for('boards'))
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now registered!')
+        login_user(user)  # Automatically log the user in after registration
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid email or password')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/boards')
+@login_required
+def boards():
+    user_boards = Board.query.filter_by(user_id=current_user.id).all()
+    return render_template("boards.html", all_boards=user_boards)
+
 
 @app.route('/create_board', methods=['GET', 'POST'])
+@login_required
 def create_board():
     form = BoardForm()
     if form.validate_on_submit():
         board_title = form.title.data
-        new_board = Board(title=board_title)
+        new_board = Board(title=board_title, user_id=current_user.id)
         
         default_columns = ["To Do", "In Progress", "Done"]
         for col_title in default_columns:
@@ -56,12 +138,18 @@ def create_board():
 
 
 @app.route('/board/<int:board_id>')
+@login_required
 def display_board(board_id):
     board = Board.query.get_or_404(board_id)
+    if board.user_id != current_user.id:
+        flash('Access denied!')
+        return redirect(url_for('boards'))
     columns = Column.query.filter_by(board_id=board_id).all()
     return render_template('board.html', board=board, columns=columns)
 
+
 @app.route('/board/<int:board_id>/add_task/<int:column_id>', methods=['GET', 'POST'])
+@login_required
 def add_task(board_id, column_id):
     form = TaskForm()
     if form.validate_on_submit():
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 class TodoForm(FlaskForm):
     task = StringField('Task', validators=[DataRequired()])
@@ -13,4 +13,18 @@ class BoardForm(FlaskForm):
 class TaskForm(FlaskForm):
     task_content = StringField('Task', validators=[DataRequired()])
     submit = SubmitField('Add Task')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
