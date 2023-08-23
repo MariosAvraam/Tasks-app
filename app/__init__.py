130dba2189faa4e3e5aedf119b1ba074371ba3ae
Marios <marios_2510@hotmail.com>
2023-08-23 14:33:11 +0300

    Refactor __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,22 +4,28 @@ from flask_login import LoginManager
 from .config import Config
 from .database import db
 
+# Create the Flask app instance
 app = Flask(__name__,
-            template_folder='../templates',  # Adjust the path to your templates folder
-            static_folder='../static')       # Adjust the path to your static folder
+            template_folder='../templates',
+            static_folder='../static')
 
+# Load configurations from the Config class
 app.config.from_object(Config)
 
+# Initialize Flask-Bootstrap
 Bootstrap5(app)
 
+# Initialize the Flask-Login extension
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    """Load a user based on its user_id."""
     from .models import User
     return db.get_or_404(User, user_id)
 
+# Initialize Flask-SQLAlchemy with the app
 db.init_app(app)
 
 # Import routes after app initialization to avoid circular imports
