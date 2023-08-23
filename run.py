7a296855e295554dffd58a5f366ae2677b018678
Marios <marios_2510@hotmail.com>
2023-08-23 15:01:17 +0300

    Refactor run.py.

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import app, db
 
 if __name__ == '__main__':
+    # Create all database tables (if they don't exist) before running the app
     with app.app_context():
         db.create_all()
+    # Run the Flask application with debugging enabled
     app.run(debug=True)
