1350b15ee46dbd1139b718caf95c85cd52d73680
Marios <marios_2510@hotmail.com>
2023-08-23 16:17:51 +0300

    Edit config.

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     """Configuration class for the Flask app."""
-    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = getenv("DB_URI", "sqlite:///tasks.db")
     SECRET_KEY = getenv("SECRET_KEY")
