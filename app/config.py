60edfb909e2883b0612a0b49955703ac40d49a71
Marios <marios_2510@hotmail.com>
2023-08-23 14:33:37 +0300

    Refactor config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
+    """Configuration class for the Flask app."""
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
     SECRET_KEY = 'Secret_Key'
-    # Add any other configurations here
