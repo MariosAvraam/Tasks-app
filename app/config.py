5cd337ecad6737a2f209153d5cda5882005c15c5
Marios <marios_2510@hotmail.com>
2023-08-23 15:36:30 +0300

    Get secret_key from .env.

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     """Configuration class for the Flask app."""
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
-    SECRET_KEY = 'Secret_Key'
+    SECRET_KEY = getenv("SECRET_KEY")
